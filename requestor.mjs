98cc1a94a20bbbacac5b0fd9f8b2ed0af9bf5f18
Ben Hanna <benpaulhanna@gmail.com>
2025-10-16 09:46:48 -0400

    Updated to new pattern

--- requestor.mjs
+++ requestor.mjs
@@ -1,61 +1,49 @@
 import { TaskExecutor } from "@golem-sdk/task-executor";
 import { pinoPrettyLogger } from "@golem-sdk/pino-logger";
-import { OfferProposalFilterFactory } from "@golem-sdk/golem-js";
+import { GolemNetwork, OfferProposalFilterFactory } from "@golem-sdk/golem-js";
 
-const whiteListNames = ["imperfect-lunchroom"];
+const whiteListIds = ["0x2f27061fc2e4f16d2f4cd99f93fbb653b46e2ecb"];
 
-(async () => {
-  const executor = await TaskExecutor.create({
+(async function main() {
+  const glm = new GolemNetwork({
     logger: pinoPrettyLogger({ level: "info" }),
     api: { key: "03ced07404254a00ad00f52613060551" },
     // Optional: select payment network.
     // Testnets:  holesky (default), sepolia, rinkeby, amoy
     // Mainnets:  mainnet, polygon
     payment: { network: "hoodi" },
-    demand: {
-      workload: {
-        imageTag: "golem/node:20-alpine",
-      },
-    },
-    market: {
-      rentHours: 1.0,
-      pricing: {
-        model: "linear",
-        maxStartPrice: 1.0,
-        maxCpuPerHourPrice: 100.0,
-        maxEnvPerHourPrice: 5.0,
-      },
-      offerProposalFilter: OfferProposalFilterFactory.allowProvidersByName(whiteListNames),
-    },
   });
   try {
-    const result = await executor.run(async (exe) => {
-      // Example using sleep in bash commands
-      console.log("Starting long-running task...");
-      
-      // Sleep for 30 seconds then check node version
-      let remoteProcess = await exe.runAndStream(`
-        echo "Task started at $(date)"
-        sleep 5s
-        echo "After 5 second delay at $(date)"
-        sleep 10s
-        echo "After 10 second delay at $(date)"
-        node -v
-      `);
-    
-      remoteProcess.stderr.subscribe((data) => console.error("stderr: ", data));
+    await glm.connect();
 
-      await new Promise((resolve) => {
-        remoteProcess.stdout.subscribe({
-          next: (data) => console.log("stdout: ", data),
-          complete: () => resolve(),
-        });
-      });
+    const rental = await glm.oneOf({
+      order: {
+        demand: {
+          workload: { imageTag: "golem/alpine:latest" },
+        },
+        // You have to be now explicit about about your terms and expectations from the market
+        market: {
+          rentHours: 15 / 60,
+          pricing: {
+            model: "linear",
+            maxStartPrice: 1.0,
+            maxCpuPerHourPrice: 10.0,
+            maxEnvPerHourPrice: 5.0,
+          },
+          //offerProposalFilter: OfferProposalFilterFactory.allowProvidersById(whiteListIds),
+        },
+      },
     });
-    console.log("Task result:", result);
-  } catch (err) {
-    console.error("An error occurred:", err);
+
+    // You will work with exe-unit objects instead of "executor"
+    await rental
+      .getExeUnit()
+      .then((exe) => exe.run("echo 'Hello World'"))
+      .then((res) => console.log(res.stdout));
+
+  } catch (error) {
+    console.error("Failed to execute work:", error);
   } finally {
-    await executor.shutdown();
+    await glm.disconnect();
   }
 })();
